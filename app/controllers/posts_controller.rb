50be28016557f882ce5ee2a856eda07462d851d6
shotaro-kobayashi <shotaro.kobayashi.9x@stu.hosei.ac.jp>
2020-09-08 10:13:31 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
   end
+ 
   
 end
